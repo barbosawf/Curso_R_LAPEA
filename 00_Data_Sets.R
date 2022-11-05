6af946bc01194240b8779179362a26b662ff5617
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2022-11-05 00:32:25 -0300

    Adicionando

--- 00_Data_Sets.R
+++ 00_Data_Sets.R
@@ -47,5 +47,26 @@ txhousing %>%
 
 write_csv(tx_housing, file = "tx_housing.csv")
 
+df_ms <- tibble(
+y = c(64, 71, 53, 67, 55, 58, 77, 57, 56, 51, 76, 68),
+x1 = c(57, 59, 49, 62, 51, 50, 55, 48, 42, 42, 61, 57),
+x2 = c(8, 10, 6, 11, 8, 7, 10, 9, 10, 6, 12, 9),
+x3 = c(64, 100, 36, 121, 64, 49, 100, 81, 100, 36, 144, 81)
+)
+
+write_csv(df_ms, file = "df_ms.csv")
+
+
+#dados<-read.table("dados extras/selecao modelos dados draper e smith 1981 usado apostila.txt",h=T)
+df_ms_2 <- tibble(
+y = c(78.5, 74.3, 104.3, 87.6, 95.9, 109.2, 102.7,
+      72.5, 93.1, 115.9, 83.8, 113.3, 109.4),
+x1 = c(7, 1, 11, 11,  7, 11,  3,  1,  2, 21,  1, 11, 10),
+x2 = c(26, 29, 56, 31, 52, 55, 71, 31, 54, 47, 40, 66, 68),
+x3 = c(6, 15,  8,  8,  6,  9, 17, 22, 18,  4, 23,  9,  8),
+x4 = c(60, 52, 20, 47, 33, 22,  6, 44, 22, 26, 34, 12, 12))
+
+write_csv(df_ms_2, file = "df_ms_2.csv")
+
 #links
 #https://daranzolin.github.io/2016-12-10-join-list-dataframes/
--- 3rd_day.R
+++ 3rd_day.R
@@ -1,8 +1,11 @@
 # Regressão Linear Múltipla (Análise de Resíduos)
 
 # Carregamento dos pacotes
-library(tidyverse)
-library(faraway) # Belsley, D., Kuh. E. and Welsch, R. (1980) "Regression Diagnostics" Wiley.
+library(tidyverse) # Para manipulação
+library(faraway) # Contém banco de dados e funções importantes para o diagnóstico de modelos
+library(leaps) # Utilizado para seleção de variáveis no modelo
+library(lmtest) # Para o teste de correlação temporal
+library(flextable) # Para mostrar tabela personalizada
 
 ## Dados (savings)
 data(savings) # Função data() importa o banco de dados savings do pacote faraway
@@ -125,9 +128,8 @@ par(mfrow = c(1, 1))
 
 ### Gráficos ilustrativos para diagnosticar a homogeneidade de variâncias
 
-
 ## Forma original das distribuções
-dev.off()
+
 par(mfrow = c(2, 2))
 
 # - caso 1: Normal
@@ -184,15 +186,16 @@ shapiro.test(rcauchy(50))
 curve(dnorm(x), -5, 5)
 curve(dcauchy(x), -5, 5, add = T, col = "blue")
 
-
 ## Cuidado quando n for pequeno
-for (i in 1:10) {
+
+for (i in 1:15) {
   print(shapiro.test(rcauchy(5))$p.value)
 }
 
-for (i in 1:10) {
-  print(shapiro.test(rcauchy(5))$p.value)
+for (i in 1:15) {
+  print(shapiro.test(rnorm(5))$p.value)
 } 
+
 
 # Dados correlacionados
 
@@ -230,14 +233,25 @@ summary(lm(residuals(mal)[-1] ~ -1 + residuals(mal)[-153])) #
 
 # Vejamos um teste formal: aplicando o DW
 
-library(lmtest)
+
 dwtest(Ozone ~ . - Day, data = na.omit(airquality))
 
 "Observamos que não há evidência de correlação, Porém, cuidado, pois há
 valores faltantes."
 
-
 # Regressão Linear Múltipla (Multicolinearidade)
+
+## Multicolinearidade perfeita 
+(X <- matrix(c(1,2,3,2,4,6,7,5,2), 3, 3)) # relação linear exata em duas colunas
+(XtX <- t(X)%*%X)
+solve(XtX) # Não é invertível, pois o determinante dá zero
+MASS::ginv(XtX) # Usa-se inversa generalizada
+
+(X <- matrix(sample(1:9,9), 3, 3))
+(XtX <- t(X)%*%X)
+solve(XtX)
+
+
 ## Carregamento do banco de dados
 
 data(seatpos)
@@ -249,24 +263,26 @@ help("seatpos")
 
 ## Ajuste do modelo
 g <- lm(hipcenter ~ ., seatpos) # ou indica cada preditor de interesse
+
+cat("Vemos, no quadro anterior, alguns sinais de multicolinearidade: 
+nenhum preditor é significativo, mas R2 é relativamente alto.")
+
+### Resumo da análise
 summary(g)
 
 cat("Vemos, no quadro anterior, alguns sinais de multicolinearidade: \nnenhum preditor é significativo, mas R2 é relativamente alto.")
 
-##
-library(flextable)
+
+### Análise de variância
+anova(g)
+
+
+### Correlação duas a duas
+(tab_cor <- cor(seatpos) |> round(3))
+tab_cor[upper.tri(tab_cor, diag = T)] <- NA
+tab_cor # correlações
 
 library(flextable)
-
-
-data("seatpos")
-
-tab_cor <- round(cor(seatpos), 3)
-
-tab_cor[upper.tri(tab_cor)] <- NA
-
-diag(tab_cor) <- NA
-
 tab_cor |>
   as_tibble(rownames = NA) |>
   rownames_to_column("Variável") |>
@@ -279,5 +295,228 @@ tab_cor |>
       out[x > .9] <- "red"
       out
     }
-  )
+  ) 
 
+## Autovalores
+
+(X <- model.matrix(g)[,-1]) # obtenção da matriz x
+
+e <- eigen(cor(X)) # função eigen() retorna autovalores e autovetores
+e
+
+## Número de condição
+max(e$values)/min(e$values) #critério NC de Montgomery e Peck (1981)? 
+
+sqrt(max(e$values)/min(e$values)) #Se >=30 indica problema
+
+## Vif para cada variável
+### Cálculos manuais do vif
+a <- summary(lm(X[,1] ~ X[,-1]))$r.squared
+a
+1/(1-a)
+
+
+#matriz de correlação
+m.cor <- cor(X)
+#solve(m.cor)
+diag(solve(m.cor))
+
+### Cálculo do vif pela função do pacote faraway
+vif(g) # Interpretação: pode-se interpretar sqrt(307.4) = 17.5 como nos dizendo que o erro padrão para a variável HtShoes é 17.5 vezes maior do que teria sido sem multicolinearidade.
+
+# Formas de tratar a multicolinearidade
+
+## Eliminando variáveis muito correlacionadas
+g2 <- lm(hipcenter ~ . - HtShoes - Ht, seatpos)
+summary(g2)
+
+### Cálculo do NC
+X <- model.matrix(g2)[,-1] # obtenção da matriz x
+
+e <- eigen(cor(X)) # função eigen() retorna autovalores e autovetores
+
+max(e$values)/min(e$values) #critério NC de Montgomery e Peck (1981)? 
+
+sqrt(max(e$values)/min(e$values)) #Se >=30 indica problema
+
+### Cálculo do vif
+vif(g2) 
+
+### Faz sentido tirar as variáveis HtShoes e Ht do modelo?
+help(seatpos)
+
+
+# Regressão Linear Múltipla (Seleção de variáveis)
+## Carregando o banco de dados
+df_ms <- read_csv(file = "df_ms.csv", show_col_types = F)
+
+# 
+m123 <- lm(y ~ ., df_ms)
+
+m123 |> anova()
+
+X <- as.matrix(df_ms[, -1])
+y <- as.matrix(df_ms[, 1])
+
+m123 <- lm(y ~ X) 
+
+# (sum(y^2) - sum(y)^2/length(y))/(length(y) - 1)
+SSTotal <- var(y) * (length(y) - 1)
+SSTotal
+
+## Cálculo do R2
+R2 <- 693.06 / (693.06 + 195.19)
+#ou R2<-693.06/SSTotal
+R2
+
+## Cálculo do R2 ajustado
+n <- length(y)
+p = 4 #nesse caso em particular
+1 - ((n - 1) / (n - p)) * (1 - R2)
+
+#ou
+1 - (24.399 / (888.25 / 11))
+
+# ou
+summary(m123)
+
+Cp <- 195.19 / 24.399 - (n - 2 * p)
+Cp
+
+
+b <- regsubsets(y ~ ., data = df_ms, nbest = 3)
+rs <- summary(b)
+
+rs$which
+new_rs <- list()
+
+for (i in 1:length(rs)) {
+  if (mode(rs[[i]]) == "numeric") {
+    new_rs[[names(rs[i])]] <- rs[[i]]
+  }
+}
+
+(bind_cols(new_rs) |>
+  add_column(rs$outmat |> as_tibble(),
+             .before = T) -> est_models)
+
+est_models |> 
+  flextable() |>
+  bg(
+    j = c(4, 6),
+    bg = function(x) {
+      out <- rep("transparent", length(x))
+      out[x == max(x)] <- "lightgreen"
+      out
+    }
+  ) |>
+  bg(
+    j = c(5, 7,8),
+    bg = function(x) {
+      out <- rep("transparent", length(x))
+      out[x == min(x)] <- "lightblue"
+      out
+    }
+  ) 
+
+# Regressão Linear Múltipla (Backward, Forward, stepwise)
+## Carregando o banco de dados
+df_ms_2 <- read_csv(file = "df_ms_2.csv", show_col_types = F)
+df_ms_2
+
+## BACKWARD ELIMINATION
+m1234 <- lm(y ~ x1 + x2 + x3 + x4, data = df_ms_2)
+m1243 <- lm(y ~ x1 + x2 + x4 + x3, data = df_ms_2)
+m1432 <- lm(y ~ x1 + x4 + x3 + x2, data = df_ms_2)
+m4321 <- lm(y ~ x4 + x2 + x3 + x1, data = df_ms_2)
+
+### Problema: os testes F são sequenciais e não parciais
+anova(m1234)  # depende da ordem de entrada no modelo
+
+anova(m1243)
+
+anova(m1432)
+
+anova(m4321)
+
+summary(m1234) # O teste t é parcial
+
+summary(m1243)
+
+summary(m1432)
+
+summary(m4321)
+
+"observe que os testes t correspondem ao teste F parcial para a última
+ variável que entrou no modelo.
+ conclusão: melhor usar os testes t parciais"
+
+### vejamos o t tabelado a 5% com  8 g.l.
+qt(.975,8)
+
+### vejamos o F tabelado a 5% com 1 e 8 g.l.
+qf(.95,1,8)
+
+"Eliminamos a variável X3 pois possui o menor Fcalc, ou o menor tcalc, ou, alternativamente, o maior pvalor.
+
+OBS.: Veja a função Anova() no pacote car que fornece SStipo II library(car) Anova(m1234,type="II") #observe que é equivalente a summary()
+
+Recalcular a nova equação de regressão sem a X3
+
+Realizando os F parciais"
+
+car::Anova(m1234, type = 2)  # depende da ordem de entrada no modelo
+
+car::Anova(m4321, type = 2)
+
+m124 <- lm(y ~ x1 + x2 + x4, data = dados)
+anova(m124)
+
+
+## FORWARD SELECTION
+### Obter a variável com a maior correlação com Y.
+
+(cor_df_ms_2 <- cor(df_ms_2)) # Assim, a variável X4 entrará primeiro no modelo.
+
+
+m4 <- lm(y ~ x4, data = df_ms_2)
+anova(m4) # Anova
+
+summary(m4) # Resumo
+
+### Acrescentar uma variável por vez e verificar o p-valor
+
+m14 <- lm(y ~ x4 + x1, data = df_ms_2)
+anova(m14)
+
+m24 <- lm(y ~ x4 + x2, data = df_ms_2)
+anova(m24)
+
+m34 <- lm(y ~ x4 + x3, data = df_ms_2)
+anova(m34)
+
+### Depois de escolhido a segunda variável, acrescentar mais uma por vez
+m124 <- lm(y ~ x1 + x4 + x2, data = df_ms_2)
+anova(m124) # p valor > 0.05
+
+m134 <- lm(y ~ x1 + x4 + x3, data = df_ms_2)
+anova(m134) # p valor > 0.05
+
+### Modelo escolhido foi o m14
+coef(m14)
+
+## Funções prontas
+library(MASS)
+
+(step_forward <- step(m1234, direction = "forward"))
+summary(step_forward)
+step_forward$anova
+
+(step_backward <- step(m1234, direction = "backward"))
+summary(step_backward)
+step_backward$anova
+
+
+(step_both <- step(m1234, direction = "both"))
+summary(step_both)
+step_both$anova
