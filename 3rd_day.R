1961853d0e54715293ce30128369da59a1a92090
Wagner Faria Barbosa <wagner.barbosa@ufv.br>
2022-11-06 18:54:19 -0300

    update

--- 3rd_day.R
+++ 3rd_day.R
@@ -7,32 +7,44 @@ library(leaps) # Utilizado para seleção de variáveis no modelo
 library(lmtest) # Para o teste de correlação temporal
 library(flextable) # Para mostrar tabela personalizada
 
-## Dados (savings)
-data(savings) # Função data() importa o banco de dados savings do pacote faraway
-help(savings) # Mostra a descrição dos dados no help
-savings <- savings |> 
-  as_tibble() # Transforma em tibble
-savings
-
-## Dados (gala)
-data(gala) # Função data() importa o banco de dados savings do pacote faraway
-help(gala) # Mostra a descrição dos dados no help
-gala <- gala |> 
-  as_tibble() # Transforma em tibble
-gala
-
 ## Ajuste do modelo
 
+### Dados (savings)
+data(savings) # Função data() importa o banco de dados savings do pacote faraway
+
+help(savings) # Mostra a descrição dos dados no help
+
+savings <- savings |> 
+  as_tibble() # Transforma em tibble
+
+savings
+
+
+### Forma didática
+(y <- as.matrix(savings[, 1]))
+
+(X <- as.matrix(data.frame(Intercept = 1, savings[, -1])))
+
+(XtX <- t(X)%*%X)
+
+(inv_XtX <- solve(XtX))
+
+(B <- inv_XtX%*%t(X)%*%y)
+
+### Forma prática
 ms <- lm(sr ~ pop15 + pop75 + dpi + ddpi, savings)
+coef(ms)
 summary(ms)
 
 ms <- lm(sr ~ ., savings)
+coef(ms)
 summary(ms)
 
 ## Diagnótico da regressão
 plot(fitted(ms),residuals(ms), xlab = "Fitted", ylab = "Residuals")
 
 ### Resíduos x valores ajustados
+#### Homogeneidade das variâncias
 tibble(fit = fitted(ms), 
        res = residuals(ms), 
        abs_res = residuals(ms) %>% abs()) -> d_s
@@ -44,12 +56,11 @@ d_s |>
   labs(x = "Fitted", y = "Residuals") +
   theme_classic()
 
-### Resíduos x x_i
+### Resíduos versus x_i
 plot(savings$pop15, residuals(m), xlab = "Population under 15", ylab = "Residuals")
 plot(savings$pop75, residuals(m), xlab = "Population over 75", ylab = "Residuals")
 
-### Homogeneidade das variâncias
-
+### Resíduos absolutos vessus x_i
 lm(abs_res ~ fit, d_s) %>% 
   summary()
 plot(abs_res ~ fit, d_s, xlab = "Fitted", ylab = "|Residuals|")
@@ -62,25 +73,28 @@ par(mfrow = c(2, 2))
 
 # - caso 1: homogeneidade de variâncias
 for (i in 1:graficos) {
-  plot(1:50, rnorm(50), ylab = "Residuals", main = "caso 1")
+  plot(1:50, rnorm(50), xlab = expression(hat(y)), ylab = "Resíduos", main = "caso 1")
   abline(h = 0)
 }
 
 # - caso 2: heterogeneidade forte
 for (i in 1:graficos) {
-  plot(1:50, (1:50) * rnorm(50), ylab = "Residuals", main = "caso 2")
+  plot(1:50, (1:50) * rnorm(50), 
+       xlab = expression(hat(y)), ylab = "|Resíduos|", main = "caso 2")
   abline(h = 0)
 }
 
 # - caso 3: heterogeneidade moderada
 for (i in 1:graficos) {
-  plot(1:50, sqrt((1:50)) * rnorm(50), ylab = "Residuals", main = "caso 3")
+  plot(1:50, sqrt((1:50)) * rnorm(50), 
+       xlab = expression(hat(y)), ylab = "REsíduos", main = "caso 3")
   abline(h = 0)
 }
 
 # - caso 4: não-linearidade
 for (i in 1:graficos) {
-  plot(1:50, cos((1:50) * pi / 25) + rnorm(50), ylab = "Residuals", main = "caso 4")
+  plot(1:50, cos((1:50) * pi / 25) + rnorm(50), 
+       xlab = expression(hat(y)), ylab = "Resíduos", main = "caso 4")
   abline(h = 0)
 }
 par(mfrow = c(1, 1))
@@ -88,32 +102,45 @@ par(mfrow = c(1, 1))
 
 ## Transformação de y
 
-mg <- lm(Species ~ . - Endemics, gala)
+### Dados (gala)
+data(gala) # Função data() importa o banco de dados gala do pacote faraway
+help(gala) # Mostra a descrição dos dados no help
+gala <- gala |> 
+  as_tibble() # Transforma em tibble
+gala # Diversidade de espécies nas ilhas Galápagos
+
+(mg <- lm(Species ~ . - Endemics, gala))
 
 tibble(fit = fitted(mg), 
        res = residuals(mg), 
        abs_res = residuals(mg) %>% abs()) -> d_g
-
 
 par(mfrow = c(1, 1))
 plot(res ~ fit, data = d_g, 
      xlab = "Fitted", ylab = "Residuals")
 
 
-# Tranformando y em raiz
-
-mg_t <- lm(sqrt(Species) ~ . - Endemics, data = gala)
+### Tranformando y em raiz
+#### Resíduos versus valores ajustados
+(mg_t <- lm(sqrt(Species) ~ . - Endemics, data = gala))
 plot(
-  x = fitted(mg_t),
-  y = residuals(mg_t),
-  xlab = "Fitted",
-  ylab = "Residuals",
+  x = fitted(mg_t), y = residuals(mg_t),
+  xlab = "Fitted", ylab = "Residuals",
+  main = "Usando raiz quadrada")
+
+#### Resíduos absolutos versus valores ajustados
+plot(
+  x = fitted(mg_t), y = abs(residuals(mg_t)),
+  xlab = "Fitted",  ylab = "|Residuals|",
   main = "Usando raiz quadrada"
 )
 
+res_abs_x_aj <- lm(abs(residuals(mg_t)) ~ fitted(mg_t))
+abline(res_abs_x_aj)
+anova(res_abs_x_aj)
 
-# Verificando normalidade
-
+## Verificando normalidade (savings)
+par(mfrow = c(1, 1))
 qqnorm(residuals(ms), ylab = "Residuals")
 qqline(residuals(ms))
 
@@ -124,52 +151,52 @@ hist(residuals(ms))
 boxplot(residuals(ms), horizontal = T)
 par(mfrow = c(1, 1))
 
-dev.off()
+
 ### Gráficos ilustrativos para diagnosticar a homogeneidade de variâncias
 
-## Forma original das distribuções
+#### Forma original das distribuções
 
 par(mfrow = c(2, 2))
 
-# - caso 1: Normal
+##### - caso 1: Normal
 curve(dnorm(x), -5, 5, ylim = c(0, 1), main = "Normal", lwd = 2) 
 
-# - caso 2: Lognormal
+##### - caso 2: Lognormal
 curve(dlnorm(x), -5, 5, ylim = c(0,1), col = "red", main = "Lognormal", lwd = 2)
 
-# - caso 3: Cauchy
+##### - caso 3: Cauchy
 curve(dcauchy(x), -5, 5, ylim = c(0,1), col = "blue", main = "Cauchy", lwd = 2)
 
-# - caso 4: Uniforme
+##### - caso 4: Uniforme
 curve(dunif(x), -5, 5, ylim = c(0,1), col = "green", main = "Uniforme", lwd = 2)
 
 
-## Residuos de cada distribuição
+#### Residuos de cada distribuição
 graficos <- 4
 par(mfrow = c(2, 2))
 
-# - caso 1: Normal
+##### - caso 1: Normal
 for (i in 1:graficos) {
   x <- rnorm(50)
   qqnorm(x, main = "Normal")
   qqline(x)
 }
 
-# - caso 2: Lognormal
+##### - caso 2: Lognormal
 for (i in 1:graficos) {
   x <- exp(rnorm(50))
   qqnorm(x, main = "Lognormal")
   qqline(x)
 }
 
-# - caso 3: Cauchy
+##### - caso 3: Cauchy
 for (i in 1:graficos) {
   x <- rcauchy(50)
   qqnorm(x, main = "Cauchy")
   qqline(x)
 }
 
-# - caso 4: Uniforme
+##### - caso 4: Uniforme
 for (i in 1:graficos) {
   x <- runif(50)
   qqnorm(x, main = "Uniforme")
@@ -178,62 +205,86 @@ for (i in 1:graficos) {
 
 par(mfrow = c(1, 1))
 
-# Teste de shapiwo-wilkis para normalidade
-
+### Teste de shapiwo-wilkis para normalidade
 shapiro.test(rnorm(50)) # 50 valores
-shapiro.test(rcauchy(50)) # 50 valores
-shapiro.test(rgamma(50, 8)) # 50 valores
-curve(dnorm(x), -5, 5)
-curve(dcauchy(x), -5, 5, add = T, col = "blue")
 
-## Cuidado quando n for pequeno
+#### Se o n for grande
+for (i in 1:15) {
+  print(shapiro.test(rnorm(5000))$p.value) # 5000 valores da normal
+}
+
+#### Se o n for pequeno
+for (i in 1:15) {
+  print(shapiro.test(rnorm(5))$p.value) # 5 valores da normal
+} 
 
 for (i in 1:15) {
-  print(shapiro.test(rcauchy(5))$p.value)
+  print(shapiro.test(rcauchy(5))$p.value) # 5 valores da cauchy
 }
 
 for (i in 1:15) {
-  print(shapiro.test(rnorm(5))$p.value)
-} 
+  print(shapiro.test(rgamma(5, 8))$p.value) # 5 valores da gamma
+}
+
+for (i in 1:15) {
+  print(shapiro.test(runif(50, 2, 10))$p.value) # 5 valores da uniforme
+}
 
 
-# Dados correlacionados
+## Dados correlacionados temporalmente
 
 data(airquality)
 head(airquality)
 pairs(airquality, panel = panel.smooth)
+
+### Ajuste do modelo com dados não transformados
 ma <- lm(Ozone ~ . - Day, data = airquality, na.action = na.exclude)
 plot(fitted(ma), residuals(ma), xlab = "Fitted", ylab = "Residuals")
 
 "Observa-se que não há constância na variância e também não há não linearidade.
 Assim, pode-se fazer uma transformação que, neste caso, será logarítmica.
 "
+#### Checando se os erros são correlacionados
 
+plot(residuals(ma), ylab = "Residuos")
+abline(h = 0)
+lines(residuals(ma), ylab = "Residuos")
+
+#### Plotando resíduos sucessivos
+
+plot(residuals(ma)[-153],
+     residuals(ma)[-1],
+     xlab = expression(hat(epsilon)[i]),
+     ylab = expression(hat(epsilon)[i + 1]))
+
+#### teste simples (modelo sem intercepto pois resíduos tem média zero)
+summary(lm(residuals(mal)[-1] ~ -1 + residuals(mal)[-153])) # 
+"Parece não haver correlação"
+
+### Ajuste do modelo com dados não transformados
 mal <- lm(log(Ozone) ~ . - Day, data = airquality, na.action = na.exclude)
 plot(fitted(mal), residuals(mal), xlab = "Fitted", ylab = "Residuals")
 
-## Checando se os erros são correlacionados
+#### Checando se os erros são correlacionados
 
 plot(residuals(mal), ylab = "Residuos")
 abline(h = 0)
 lines(residuals(mal), ylab = "Residuos")
 
 
-## Plotando resíduos sucessivos
+#### Plotando resíduos sucessivos
 x11()
 plot(residuals(mal)[-153],
      residuals(mal)[-1],
      xlab = expression(hat(epsilon)[i]),
      ylab = expression(hat(epsilon)[i + 1]))
 
-## teste simples (modelo sem intercepto pois resíduos tem média zero)
+#### teste simples (modelo sem intercepto pois resíduos tem média zero)
 summary(lm(residuals(mal)[-1] ~ -1 + residuals(mal)[-153])) # 
 "Parece não haver correlação"
 
 
 # Vejamos um teste formal: aplicando o DW
-
-
 dwtest(Ozone ~ . - Day, data = na.omit(airquality))
 
 "Observamos que não há evidência de correlação, Porém, cuidado, pois há
@@ -253,10 +304,8 @@ solve(XtX)
 
 
 ## Carregamento do banco de dados
-
 data(seatpos)
-seatpos <- seatpos |> as_tibble()
-seatpos
+(seatpos <- seatpos |> as_tibble())
 
 ## Estudo da posição do assento do motorista em função de vários preditores
 help("seatpos")
@@ -264,18 +313,13 @@ help("seatpos")
 ## Ajuste do modelo
 g <- lm(hipcenter ~ ., seatpos) # ou indica cada preditor de interesse
 
-cat("Vemos, no quadro anterior, alguns sinais de multicolinearidade: 
-nenhum preditor é significativo, mas R2 é relativamente alto.")
-
 ### Resumo da análise
 summary(g)
 
-cat("Vemos, no quadro anterior, alguns sinais de multicolinearidade: \nnenhum preditor é significativo, mas R2 é relativamente alto.")
-
-
 ### Análise de variância
-anova(g)
+car::Anova(g)
 
+#### Vemos, no quadro anterior, alguns sinais de multicolinearidade: nenhum preditor é significativo, mas R2 é relativamente alto.
 
 ### Correlação duas a duas
 (tab_cor <- cor(seatpos) |> round(3))
@@ -295,7 +339,7 @@ tab_cor |>
       out[x > .9] <- "red"
       out
     }
-  ) 
+  )
 
 ## Autovalores
 
@@ -307,22 +351,24 @@ e
 ## Número de condição
 max(e$values)/min(e$values) #critério NC de Montgomery e Peck (1981)? 
 
-sqrt(max(e$values)/min(e$values)) #Se >=30 indica problema
+#sqrt(max(e$values)/min(e$values)) #Se >=30 indica problema
 
 ## Vif para cada variável
 ### Cálculos manuais do vif
-a <- summary(lm(X[,1] ~ X[,-1]))$r.squared
-a
-1/(1-a)
+for (i in 1:ncol(X)) {
+  R2 <- summary(lm(X[,i] ~ X[,-i]))$r.squared
+  print(1/(1-R2))
+}
 
-
-#matriz de correlação
+### Diagonal da inversa da matriz de correlação
 m.cor <- cor(X)
-#solve(m.cor)
+
 diag(solve(m.cor))
 
 ### Cálculo do vif pela função do pacote faraway
-vif(g) # Interpretação: pode-se interpretar sqrt(307.4) = 17.5 como nos dizendo que o erro padrão para a variável HtShoes é 17.5 vezes maior do que teria sido sem multicolinearidade.
+vif(g) 
+
+#### Interpretação: pode-se interpretar sqrt(307.4) = 17.5 como nos dizendo que o erro padrão para a variável HtShoes é 17.5 vezes maior do que teria sido sem multicolinearidade.
 
 # Formas de tratar a multicolinearidade
 
@@ -350,44 +396,17 @@ help(seatpos)
 ## Carregando o banco de dados
 df_ms <- read_csv(file = "df_ms.csv", show_col_types = F)
 
-# 
+## Ajuste do modelo com todas as variáveis
 m123 <- lm(y ~ ., df_ms)
 
-m123 |> anova()
+car::Anova(m123) # apenas um preditor é significativo
 
-X <- as.matrix(df_ms[, -1])
-y <- as.matrix(df_ms[, 1])
-
-m123 <- lm(y ~ X) 
-
-# (sum(y^2) - sum(y)^2/length(y))/(length(y) - 1)
-SSTotal <- var(y) * (length(y) - 1)
-SSTotal
-
-## Cálculo do R2
-R2 <- 693.06 / (693.06 + 195.19)
-#ou R2<-693.06/SSTotal
-R2
-
-## Cálculo do R2 ajustado
-n <- length(y)
-p = 4 #nesse caso em particular
-1 - ((n - 1) / (n - p)) * (1 - R2)
-
-#ou
-1 - (24.399 / (888.25 / 11))
-
-# ou
-summary(m123)
-
-Cp <- 195.19 / 24.399 - (n - 2 * p)
-Cp
-
-
+## Ajute de todas as possíveis regressões
 b <- regsubsets(y ~ ., data = df_ms, nbest = 3)
+
+### Extraindo as estatísticas de cada modelo
 rs <- summary(b)
 
-rs$which
 new_rs <- list()
 
 for (i in 1:length(rs)) {
@@ -396,28 +415,23 @@ for (i in 1:length(rs)) {
   }
 }
 
+### Mostrando as estatísticas de todos os modelos
 (bind_cols(new_rs) |>
   add_column(rs$outmat |> as_tibble(),
              .before = T) -> est_models)
 
 est_models |> 
   flextable() |>
-  bg(
-    j = c(4, 6),
+  bg(j = c(4, 6),
     bg = function(x) {
       out <- rep("transparent", length(x))
       out[x == max(x)] <- "lightgreen"
-      out
-    }
-  ) |>
-  bg(
-    j = c(5, 7,8),
+      out } ) |>
+  bg(j = c(5, 7,8),
     bg = function(x) {
       out <- rep("transparent", length(x))
       out[x == min(x)] <- "lightblue"
-      out
-    }
-  ) 
+      out}) 
 
 # Regressão Linear Múltipla (Backward, Forward, stepwise)
 ## Carregando o banco de dados
@@ -426,53 +440,36 @@ df_ms_2
 
 ## BACKWARD ELIMINATION
 m1234 <- lm(y ~ x1 + x2 + x3 + x4, data = df_ms_2)
-m1243 <- lm(y ~ x1 + x2 + x4 + x3, data = df_ms_2)
-m1432 <- lm(y ~ x1 + x4 + x3 + x2, data = df_ms_2)
 m4321 <- lm(y ~ x4 + x2 + x3 + x1, data = df_ms_2)
 
 ### Os testes F da função anova do pacote stats são sequenciais e não parciais
 #### Dependem da ordem de entrada no modelo
 anova(m1234)  
-
-anova(m4321)
+anova(m4321) # A ordem importa
 
 ### Os testes t da função summary do pacote base não parciais
 #### Não dependem da ordem de entrada no modelo
 summary(m1234) 
-
 summary(m4321)
-
-"observe que os testes t correspondem ao teste F parcial para a última
- variável que entrou no modelo.
- conclusão: melhor usar os testes t parciais"
-
-anova(m1234) " <=> "; summary(m1234) "apenas para última variável"
 
 ### Os testes F da função Anova do pacote car são parciais
 #### Não dependem da ordem de entrada no modelo
-
 car::Anova(m1234) 
-
 car::Anova(m4321)
 
 ### Recalcular a nova equação de regressão sem a X3
-
 m124 <- lm(y ~ x1 + x2 + x4, data = df_ms_2)
 car::Anova(m124) 
 
 ### Recalcular a nova equação de regressão sem a X4
-car::Anova(m4321, type = 2)
-
 m12 <- lm(y ~ x1 + x2, data = df_ms_2)
 car::Anova(m12)
-
 summary(m12)
 
 ## FORWARD SELECTION
 ### Obter a variável com a maior correlação com Y.
 
 (cor_df_ms_2 <- cor(df_ms_2)) # Assim, a variável X4 entrará primeiro no modelo.
-
 
 m4 <- lm(y ~ x4, data = df_ms_2)
 anova(m4) # Anova
